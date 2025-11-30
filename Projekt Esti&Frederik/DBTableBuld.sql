514cade17881cca0247f5447600e9e5488252b3d
Elvie-Abarth <estibrusse18@gmail.com>
2025-11-30 14:36:02 +0100

    Updaddet DBTableBuld (fixd errors) added constraint.

--- Projekt Esti&Frederik/DBTableBuld.sql
+++ Projekt Esti&Frederik/DBTableBuld.sql
@@ -1,5 +1,10 @@
 ﻿use ExamPlannerDB;
 
+drop table if exists Teachers;
+drop table if exists Classes;
+drop table if exists Exams;
+drop table if exists Designations;
+
 create table Teachers (
 	TeacherID int primary key identity(1,1) not null,
 	FirstName varchar(50) not null,
@@ -8,22 +13,34 @@ create table Teachers (
 	Email varchar(100) unique not null
 );
 
+create table Classes (
+	ClassID int primary key identity(1,1) not null,
+	ClassName varchar(100) not null
+);
+
 create table Exams (
 	ExamID int primary key identity(1,1) not null,
-	ExamName varchar(100) not null,
-	ExamDate date not null,
-	StartTime time not null,
-	EndTime time not null,
+	ClassID int foreign key references Classes(ClassID),
+	ExamName varchar(100) not null,	
 	EstimatedNumberStudents int not null,
-	TypeOfExam varchar(1) not null,
-	Supervision boolean not null,
+	TypeOfExam varchar(1) not null, -- 'E'(External) or 'I'(Internal)
+	Supervision bit not null,  -- 1 = Yes, 0 = No
 	TypeOfSupervisor varchar(50) not null,
 	ExamSubmissionDate date not null,
+	ExamDate date not null,
+	ExamStartTime time not null,
+	ExamEndTime time not null,
 	ReExamSubmissionDate date not null,
 	ReExamDate date not null,
 	ReExamStartTime time not null,
-	ReExamEndTime time not null
-
+	ReExamEndTime time not null,
+	constraint chk_EstimatedNumberStudents check (EstimatedNumberStudents > 0),
+	constraint chk_TypeOfExam check (TypeOfExam in ('E', 'I')),	
+	constraint chk_Supervision check ((Supervision = 1 and TypeOfSupervisor <> 'Yes') or (Supervision = 0 and TypeOfSupervisor = 'No')),
+	constraint chk_ReExamSubmissionDate check (ExamSubmissionDate < ReExamSubmissionDate),
+	constraint chk_ExamDate check (ExamDate < ReExamDate),	
+	constraint chk_ExamTimes check (ExamStartTime < ExamEndTime),	
+	constraint chk_ReExamTimes check (ReExamStartTime < ReExamEndTime)
 );
 
 create table Designations (
@@ -31,9 +48,4 @@ create table Designations (
 	TeacherID int foreign key references Teachers(TeacherID),
 	ExamId int foreign key references Exams(ExamID),
 	DesignationRole varchar(100) not null
-);
-
-create table Classes (
-	ClassID int primary key identity(1,1) not null,
-	ClassName varchar(100) not null
 );
