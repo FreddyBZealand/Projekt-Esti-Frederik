ecdbc341b094a5b81f587e878c5d6f8fe2ef63ae
Elvie-Abarth <estibrusse18@gmail.com>
2025-12-10 14:54:43 +0100

    Updade mockData Class. New SQLQueryUpdateClassTabel MockData.

---
+++ SQLQueryUpdateClassTabel.sql
@@ -0,0 +1,11 @@
+﻿use ExamPlannerDB;
+
+UPDATE Classes
+SET ClassName = 'DAT-RO-F-V25B' 
+WHERE ClassID = 1;
+
+use ExamPlannerDB;
+
+UPDATE Classes
+SET ClassName = 'DATO-ROS-BA-B25S' 
+WHERE ClassID = 2;
