943625de01996518e30e0f2f65d48988ccd74a37
Elvie-Abarth <estibrusse18@gmail.com>
2025-12-10 15:00:38 +0100

    Fix on typo error

--- SQLQueryUpdateClassTabel.sql
+++ SQLQueryUpdateClassTabel.sql
@@ -9,3 +9,11 @@ use ExamPlannerDB;
 UPDATE Classes
 SET ClassName = 'DATO-ROS-BA-B25S' 
 WHERE ClassID = 2;
+
+UPDATE Exams
+SET ExamName = 'DAT-RO-F-V25B' 
+WHERE ExamID = 1;
+
+UPDATE Exams
+SET ExamName = 'DATO-ROS-BA-B25S' 
+WHERE ExamID = 3;
